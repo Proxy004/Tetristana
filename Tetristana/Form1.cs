e23003acf7f2448d7321dd0f07d219d558af48ce
Behemoth <simonostini@gmail.com>
2020-11-14 11:30:32 +0100

    update

--- Tetristana/Form1.cs
+++ Tetristana/Form1.cs
@@ -16,25 +16,7 @@ namespace Tetristana
             TetrisConfig.InitializeGame(this);
 
             //init Tick methods
-            TetrisConfig.tmr_move_blocks.Tick += Tmr_move_blocks_Tick;
-
-            //Z z = new Z();
-            //z.renderShape(this);
-
-            //O o = new O();
-            //o.renderShape(this);
-
-            //T t = new T();
-            //t.renderShape(this);
-
-            //S s = new S();
-            //s.renderShape(this);
-
-            //L l = new L();
-            //l.renderShape(this);
-
-            //J j = new J();
-            //j.renderShape(this);
+            TetrisConfig.tmr_move_blocks.Tick += Tmr_move_blocks_Tick; 
         }
 
         private void Tmr_move_blocks_Tick(object sender, System.EventArgs e)
